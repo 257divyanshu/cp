2a0f7ea632371d27ce4d42880336f30079bfd1cc
257divyanshu <257divyanshu@gmail.com>
2024-04-08 21:32:21 +0530

    day1

---
+++ cpp_basics.cpp
@@ -0,0 +1,54 @@
+// WHY C++
+// - efficient and fast
+// - in-built data structures and algorithms (STL)
+
+// double has more precision than float
+// string is different and string literal (it's like an integer and 5)
+
+// \n is faster then endl
+
+// x/y when both operands are integes the result is the quotient
+// float(5)/2 will return 2.5
+// 5.0/2 will return 2.5
+
+// C++ follows BODMAS rule
+
+// in C++ we cannot strings using +
+
+// 1e5 is 1*10^5
+
+// compilation error means code didn't run
+// runtime error means code started, but error occured in between (undefined error)
+
+// cout<<"'a'"; // won't lead to any error
+
+// VARIABLES NAMES
+// - cannot have spaces
+// - cannot start with digits
+// - cannot by keywords
+
+// FLOOR
+// 1.9 -> 1
+// 1.1 -> 1
+// -1.1 -> -2
+// -1.9 -> -2
+
+// CEIL
+// 1.9 -> 2
+// 1.1 -> 2
+// -1.1 -> -1
+// -1.9 -> -1
+
+// PRIMITIVE DATATYPES
+// int char bool float void
+
+// DERIVED DATATYPE
+// set
+// map
+// vector
+// string
+// priority_queue
+
+// int x = 4;
+// cin>>x;
+// won't throw any error
---
+++ sample.cpp
@@ -0,0 +1,32 @@
+// DIV 3 CONTEST ON 08 APRIL 2024 question A
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main()
+// {
+//         int n, a, b;
+//         cin >> n >> a >> b;
+//         float B = (float)b/2; 
+//         if(B>(float)a)
+//         {
+//             // cout<<"using b"<<endl;
+//             cout<<(a*n);
+//         }
+//         else
+//         {
+//             // cout<<"using b"<<endl;
+//             cout<< round(B*n);
+//         }
+//     return 0;
+// };
+
+// DIGITS SUMMATION (Day2)
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     double N, M;
+//     cin >> N >> M;
+//     cout<<(((int)N)%10) + (((int)M)%10)<<endl;
+//     return 0;
+// };
